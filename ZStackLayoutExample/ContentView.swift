8e06beca48cd121596561c963015a0dd81c171bd
Russell Gordon <rgordon@lcs.on.ca>
2021-10-30 09:01:27 -0400

    Initial Commit

---
+++ ZStackLayoutExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ZStackLayoutExample
+//
+//  Created by Russell Gordon on 2021-10-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ZStackLayoutExample/ZStackLayoutExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZStackLayoutExampleApp.swift
+//  ZStackLayoutExample
+//
+//  Created by Russell Gordon on 2021-10-30.
+//
+
+import SwiftUI
+
+@main
+struct ZStackLayoutExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
